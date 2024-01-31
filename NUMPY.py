0dd0d30c70b5f788d07a8ea5ec8b52f54e14115e
jayesh sarvaiya <129164474+jayesh6442@users.noreply.github.com>
2024-01-31 11:59:24 +0530

    Add files via upload
    learn about the numpy library of python

---
+++ NUMPY.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+
+
+# In[3]:
+
+
+print("hello numpy")
+
+
+# In[4]:
+
+
+np.zeros(5)
+
+
+# In[5]:
+
+
+np.zeros(6,6)
+
+
+# In[6]:
+
+
+np.zeros((3,6))
+
+
+# In[7]:
+
+
+np.zeros((3,6),dtype = "int")
+
+
+# In[8]:
+
+
+np.ones((4,4),dtype= "int")
+
+
+# In[9]:
+
+
+np.ones((6,6),dtype= "int")
+
+
+# In[10]:
+
+
+np.ones((3,4,5),dtype="int")
+
+
+# In[11]:
+
+
+np.full(8,5)
+
+
+# In[12]:
+
+
+np.full((4,4),6,dtype="int")
+
+
+# In[13]:
+
+
+np.arange(5)
+
+
+# In[17]:
+
+
+examp = np.arange(3,10,dtype="float")
+
+
+# In[18]:
+
+
+print(examp)
+
+
+# In[ ]:
+
+
+np.
+
